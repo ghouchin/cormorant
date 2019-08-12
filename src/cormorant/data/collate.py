5dabc7f12b7446c6331d0800f0a226be5aaabd04
Brandon Anderson <branderson@gmail.com>
2019-08-12 17:58:41 -0500

    New batch_stack_general written. (Not added to collate_fn.)

--- src/cormorant/data/collate.py
+++ src/cormorant/data/collate.py
@@ -1,4 +1,68 @@
 import torch
+import numpy as np
+
+def batch_stack_general(props):
+    """
+    Stack a list of torch.tensors so they are padded to the size of the
+    largest tensor along each axis.
+
+    Unlike :batch_stack:, this will automatically stack scalars, vectors,
+    and matrices. It will also automatically convert Numpy Arrays to
+    Torch Tensors.
+
+    Parameters
+    ----------
+    props : list or tuple of Pytorch Tensors, Numpy ndarrays, ints or floats.
+        Pytorch tensors to stack
+
+    Returns
+    -------
+    props : Pytorch tensor
+        Stacked pytorch tensor.
+
+    Notes
+    -----
+    TODO : Review whether the behavior when elements are not tensors is safe.
+    """
+    if type(props[0]) in [int, float]:
+        # If batch is list of floats or ints, just create a new Torch Tensor.
+        return torch.tensor(props)
+
+    if type(props[0]) is np.ndarray:
+        # Convert numpy arrays to tensors
+        props = [torch.from_numpy(prop) for prop in props]
+
+    shapes = [prop.shape for prop in props]
+
+    if all(shapes[0] == shape for shape in shapes):
+        # If all shapes are the same, stack along dim=0
+        return torch.stack(props)
+
+    elif all(shapes[0][1:] == shape[1:] for shape in shapes):
+        # If shapes differ only along first axis, use the RNN pad_sequence to pad/stack.
+        return torch.nn.utils.rnn.pad_sequence(props, batch_first=True, padding_value=0)
+
+    elif all((shapes[0][2:] == shape[2:]) for shape in shapes):
+        # If shapes differ along the first two axes, (shuch as a matrix),
+        # pad/stack first two axes
+
+        # Ensure that input features are matrices
+        assert all((shape[0] == shape[1]) for shape in shapes), 'For batch stacking matrices, first two indices must match for every data point'
+
+        max_atoms = max([len(p) for p in props])
+        max_shape = (len(props), max_atoms, max_atoms) + props[0].shape[2:]
+        padded_tensor = torch.zeros(max_shape, dtype=props[0].dtype, device=props[0].device)
+
+        for idx, prop in enumerate(props):
+            this_atoms = len(prop)
+            padded_tensor[idx, :this_atoms, :this_atoms] = prop
+
+        return padded_tensor
+    else:
+        ValueError('Input tensors must have the same shape on all but at most the first two axes!')
+
+
+
 
 
 def batch_stack(props, edge_mat=False):
