9cb347ac2924122b7c4505908396205581b795eb
Erik Henning Thiede <ehthiede@gmail.com>
2020-03-03 11:34:06 -0500

    Added ASE cormorant class that takes relative position vectors as input

--- src/cormorant/models/__init__.py
+++ src/cormorant/models/__init__.py
@@ -3,5 +3,6 @@ from cormorant.models.cormorant_cg import CormorantCG
 
 from cormorant.models.cormorant_md17 import CormorantMD17
 from cormorant.models.cormorant_qm9 import CormorantQM9
+from cormorant.models.cormorant_ase import CormorantASE
 
 from cormorant.models.autotest import cormorant_tests
--- tests/models/test_Cormorant.py
+++ tests/models/test_Cormorant.py
@@ -1,10 +1,9 @@
 import pytest
 
-from cormorant.models import CormorantQM9, CormorantMD17
+from cormorant.models import CormorantQM9, CormorantMD17, CormorantASE
 
 
 class TestCormorant():
-
     @pytest.mark.parametrize('Cormorant', [CormorantQM9, CormorantMD17])
     @pytest.mark.parametrize('maxl', [1, 2, [1, 2]])
     @pytest.mark.parametrize('max_sh', [1, 2])
@@ -32,6 +31,38 @@ class TestCormorant():
                               weight_init, level_gain, charge_power, basis_set,
                               charge_scale, gaussian_mask,
                               top, input, num_mpnn_layers)
+
+        # Get data and then try pushing through a single example
+        cormorant(data)
+
+
+class TestASECormorant():
+    @pytest.mark.parametrize('cutoff_type', ['hard', 'soft', 'learn', ['hard', 'soft'], ['hard', 'learn']])
+    def test_Cormorant(self, cutoff_type, sample_batch):
+        num_channels = 4
+        level_gain = 10
+        maxl = max_sh = 2
+        data, num_species, charge_scale = sample_batch
+        data['rel_pos_vecs'] = data['positions'].unsqueeze(-2) - data['positions'].unsqueeze(-3)
+
+        num_cg_levels = 3
+        hard_cut_rad = 1.
+        soft_cut_rad = 1.
+        soft_cut_width = 1.
+        weight_init = 'rand'
+        charge_power = 2
+        basis_set = (3, 3)
+        gaussian_mask = False
+        top = 'linear'
+        input = 'linear'
+        num_mpnn_layers = 2
+
+        # First test initialization
+        cormorant = CormorantASE(maxl, max_sh, num_cg_levels, num_channels, num_species,
+                                 cutoff_type, hard_cut_rad, soft_cut_rad, soft_cut_width,
+                                 weight_init, level_gain, charge_power, basis_set,
+                                 charge_scale, gaussian_mask,
+                                 top, input, num_mpnn_layers)
 
         # Get data and then try pushing through a single example
         cormorant(data)
