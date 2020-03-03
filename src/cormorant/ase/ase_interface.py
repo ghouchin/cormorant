9d2b5ddc3b0fc7e3c99bb2c6e62c327967dd454a
Erik Henning Thiede <ehthiede@gmail.com>
2020-03-03 15:56:38 -0500

    Minor bugfixes and API changes to ase_interface

--- src/cormorant/ase/ase_interface.py
+++ src/cormorant/ase/ase_interface.py
@@ -2,11 +2,17 @@ from ase.calculators.calculator import Calculator
 from cormorant.models import CormorantASE
 from cormorant.engine import init_argparse, init_file_paths, init_logger, init_cuda, set_dataset_defaults
 from cormorant.engine import init_optimizer, init_scheduler
-from cormorant.data.utils import initialize_datasets
+from cormorant.engine import Engine
+# from cormorant.data.utils import initialize_datasets
 from cormorant.data.prepare import gen_splits_ase
 from cormorant.data.prepare.process import process_ase, process_db_row
+from cormorant.data.collate import collate_fn
+from torch.utils.data import DataLoader
 import torch
-from ase.db import connect
+import os
+import logging
+import numpy as np
+# from ase.db import connect
 
 
 class ASEInterface(Calculator):
@@ -25,25 +31,25 @@ class ASEInterface(Calculator):
 
         # Initialize device and data type
         device, dtype = init_cuda(args.cuda, args.dtype)
-        model = CormorantASE(*args, num_species=num_species,charge_scale=charge_scale,
+        model = CormorantASE(*args, num_species=num_species, charge_scale=charge_scale,
                              device=device, dtype=dtype)
         model.load_state_dict(saved_run['model_state'])
         calc = cls(model, included_species)
         return calc
 
-    def train(self, database, workdir=None):
-        # This makes printing tensors more readable.  
+    def train(self, database, workdir=None, force_factor=0.):
+        # This makes printing tensors more readable.
         torch.set_printoptions(linewidth=1000, threshold=100000)
 
-        logger = logging.getLogger('')
+        logging.getLogger('')
 
-        # Initialize arguments -- Just      
+        # Initialize arguments -- Just
         args = init_argparse('ase-db')
-        
-        if output_dir != None:
+
+        if workdir is not None:
             args.workdir = workdir
-        
-        # Initialize file paths  
+
+        # Initialize file paths
         all_files = init_file_paths(args.prefix, args.workdir, args.modeldir, args.logdir, args.predictdir, args.logfile, args.bestfile, args.checkfile, args.loadfile, args.predictfile)
 
         logfile, bestfile, checkfile, loadfile, predictfile = all_files
@@ -52,11 +58,12 @@ class ASEInterface(Calculator):
         # Initialize logger
         init_logger(logfile, args.log_level)
 
-        # Initialize device and data type     
+        # Initialize device and data type
         device, dtype = init_cuda(args.cuda, args.dtype)
 
         # Initialize dataloader
-        ntr, nv, nte, datasets, num_species, charge_scale = self.initialize_database(args.num_train, args.num_valid, args.num_test, args.datadir, database)
+        force_train = (force_factor != 0.)
+        ntr, nv, nte, datasets, num_species, charge_scale = self.initialize_database(args.num_train, args.num_valid, args.num_test, args.datadir, database, force_train=force_train)
 
         args.num_train, args.num_valid, args.num_test = ntr, nv, nte
 
@@ -68,32 +75,31 @@ class ASEInterface(Calculator):
                                          collate_fn=collate_fn)
                        for split, dataset in datasets.items()}
 
-
         model = CormorantASE(*args, device=device, dtype=dtype)
 
         # Initialize the scheduler and optimizer
         optimizer = init_optimizer(model, args.optim, args.lr_init, args.weight_decay)
         scheduler, restart_epochs = init_scheduler(optimizer, args.lr_init, args.lr_final, args.lr_decay,
-                                               args.num_epoch, args.num_train, args.batch_size, args.sgd_restart,
-                                               lr_minibatch=args.lr_minibatch, lr_decay_type=args.lr_decay_type)
+                                                   args.num_epoch, args.num_train, args.batch_size, args.sgd_restart,
+                                                   lr_minibatch=args.lr_minibatch, lr_decay_type=args.lr_decay_type)
 
         # Define a loss function. Just use L2 loss for now.
         loss_fn = torch.nn.functional.mse_loss
 
-        # Instantiate the training class  
+        # Instantiate the training class
         trainer = Engine(args, dataloaders, model, loss_fn, optimizer, scheduler, args.target, restart_epochs,
-                     bestfile=bestfile, checkfile=checkfile, num_epoch=args.num_epoch,
-                     num_train=args.num_train, batch_size=args.batch_size, device=device, dtype=dtype,
-                     save=args.save, load=args.load, alpha=args.alpha, lr_minibatch=args.lr_minibatch,
-                     textlog=args.textlog)
+                         bestfile=bestfile, checkfile=checkfile, num_epoch=args.num_epoch,
+                         num_train=args.num_train, batch_size=args.batch_size, device=device, dtype=dtype,
+                         save=args.save, load=args.load, alpha=args.alpha, lr_minibatch=args.lr_minibatch,
+                         textlog=args.textlog)
 
-        # Load from checkpoint file. If no checkpoint file exists, automatically does nothing. 
+        # Load from checkpoint file. If no checkpoint file exists, automatically does nothing.
         trainer.load_checkpoint()
         self.trainer = trainer
-        
-        # Train model.  
+
+        # Train model.
         self.trainer.trainer()
-        
+
         # Test predictions on best model and also last checkpointed model.
         self.trainer.evaluate()
 
@@ -124,94 +130,74 @@ class ASEInterface(Calculator):
             forces = self._get_forces(energy, corm_input)
             self.results['forces'] = forces
 
-    def initialize_database(self, num_train, num_valid, num_test, datadir, database):
-            """
-            Initialized the ASE database into a format that the Pytorch routines can use
+    def initialize_database(self, num_train, num_valid, num_test, datadir, database, splits=None, force_train=True):
+        """
+        Initialized the ASE database into a format that the Pytorch routines can use
 
-            Parameters
-            ----------
-            num_train: int
-                Number of training points to use
-            num_valid: int
-                Number of validation points to use
-            num_testing: int
-                Number of testing points to use
-            datadir
-                path for where the data and will be stored
-            database: str
-                path to the ASE database 
-            Returns
-            -------
-            ntr, nv, nte, datasets, num_species, charge_scale
+        Parameters
+        ----------
+        num_train: int
+            Number of training points to use
+        num_valid: int
+            Number of validation points to use
+        num_testing: int
+            Number of testing points to use
+        datadir
+            path for where the data and will be stored
+        database: str
+            path to the ASE database
+        Returns
+        -------
+        ntr, nv, nte, datasets, num_species, charge_scale
 
-            """
+        """
 
-        # Set the number of points based upon the arguments 
-        num_pts = {'train': num_train,
-               'test': num_test, 'valid': num_valid}
+        # Set the number of points based upon the arguments
+        self.num_pts = {'train': num_train, 'test': num_test, 'valid': num_valid}
 
-        ## Download and process dataset. Returns datafiles. 
-        #datafiles = prepare_dataset(datadir, dataset, subset, splits, force_download=force_download, name=db_name, path=db_path)
+        # Download and process dataset. Returns datafiles.
+        # datafiles = prepare_dataset(datadir, dataset, subset, splits, force_download=force_download, name=db_name, path=db_path)
         label = os.path.splitext(os.path.basename(database))[0]
         dataset_dir = [datadir, label]
         split_names = ['train', 'valid', 'test']
 
-        # Assume one data file for each split 
+        # Assume one data file for each split
         datafiles = {split: os.path.join(
-        *(dataset_dir + [split + '.npz'])) for split in split_names}
+            *(dataset_dir + [split + '.npz'])) for split in split_names}
 
-        # Check datafiles exist 
+        # Check datafiles exist
         datafiles_checks = [os.path.exists(datafile)
-                        for datafile in datafiles.values()]
+                            for datafile in datafiles.values()]
         # Check if prepared dataset exists, and if not set flag to download below.
         # Probably should add more consistency checks, such as number of datapoints, etc...
-        new_download = False
         if all(datafiles_checks):
             logging.info('Dataset exists and is processed.')
-        elif all(not x for x in datafiles_checks):
-            # If checks are failed.                                                                                      
-            new_download = True
         else:
-            raise ValueError(
-                'Dataset only partially processed. Try deleting {} and running again to download/process.'.format(os.path.join(dataset_dir)))
+            raise ValueError('All dataset files not found!  Make sure everything is in place.')
 
-        # If need to download dataset, pass to appropriate downloader       
-        if new_download or force_download:
-        logging.info('Dataset does not exist. Downloading!')
-        #download_dataset_ase(datadir, dataset, name, path, splits, cleanup=cleanup)
+        # Define directory for which data will be output.
+        asedir = os.path.join(*[datadir, label])
 
-        # Define directory for which data will be output. 
-        asedir = join(*[datadir, dataname, name])
-
-        # Important to avoid a race condition 
+        # Important to avoid a race condition
         os.makedirs(asedir, exist_ok=True)
 
         logging.info('Downloading and processing the given ASE Database. Output will be in directory: {}.'.format(asedir))
-        
-        # If splits are not specified, automatically generate them. 
-        if splits is None:
-            splits = gen_splits_ase(asedir, database, cleanup)
 
-        # Process ASE database, and return dictionary of splits 
+        # If splits are not specified, automatically generate them.
+        if splits is None:
+            splits = gen_splits_ase(asedir, database, cleanup=True)
+
+        # Process ASE database, and return dictionary of splits
         ase_data = {}
         for split, split_idx in splits.items():
-            ase_data[split] = process_ase(path, process_db_row, file_idx_list=split_idx, stack=True)
-
+            ase_data[split] = process_ase(database, process_db_row, file_idx_list=split_idx, stack=True, forcetrain=force_train)
 
         # Save processed ASE data into train/validation/test splits
         logging.info('Saving processed data:')
         for split, data in ase_data.items():
-            savedir = join(asedir, split+'.npz')
+            savedir = os.path.join(asedir, split+'.npz')
             np.savez_compressed(savedir, **data)
-
         logging.info('Processing/saving complete!')
-
-
-        images = get_unique_images(database)
-
-        
-
-
 
     def _get_forces(self, energy, batch):
         forces = []
@@ -237,7 +223,5 @@ class ASEInterface(Calculator):
         return data
 
 
-
-def get_unique_images(database):
-    db = connect(database)
-    
+# def get_unique_images(database):
+#     db = connect(database)
