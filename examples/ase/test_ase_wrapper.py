72f1c97f0f5e0df808cc4f591f6f63ae0353c717
Gregory Houchins <ghouchin@coe.psc.edu>
2020-03-18 19:08:05 -0400

    quick add of engine for memory reduction debugging

--- examples/ase/test_ase_wrapper.py
+++ examples/ase/test_ase_wrapper.py
@@ -1,32 +1,38 @@
 import numpy as np
 from cormorant.ase import ASEInterface
 from ase.io import read
-
-calc = ASEInterface()
-calc.train('test_force.db',force_factor=1., num_epoch=10, batch_size=3)
+#from pytorch_memlab import profile
 
 
+#@profile
+def main():
+    calc = ASEInterface()
+    calc.train('same_size.db',force_factor=1., num_epoch=4, batch_size=4)
 
 
-atoms = read('test.traj')
-e_DFT=atoms.get_potential_energy()
-f_DFT=atoms.get_forces()
-#print(e_DFT)
-#print(f_DFT)
 
-#included_species = []
-#for i in range(len(atoms)):
-#    included_species.append(atoms.numbers[i])
-#included_species = list(np.unique(included_species))
 
-#calc = ASEInterface.load('../../model/LiNMC.pt', num_species=5, included_species=included_species)
+    atoms = read('test.traj')
+    e_DFT=atoms.get_potential_energy()
+    f_DFT=atoms.get_forces()
+    #print(e_DFT)
+    #print(f_DFT)
+    
+    #included_species = []
+    #for i in range(len(atoms)):
+    #    included_species.append(atoms.numbers[i])
+    #included_species = list(np.unique(included_species))
 
-# atoms=read('/home/ghouchin/ClusterExpansion/cifs/LiNiO2.cif')
+    #calc = ASEInterface.load('../../model/LiNMC.pt', num_species=5, included_species=included_species)
 
-atoms.set_calculator(calc)
+    # atoms=read('/home/ghouchin/ClusterExpansion/cifs/LiNiO2.cif')
 
-print(atoms.get_potential_energy())
-print(atoms.get_forces())
-import pdb
-pdb.set_trace()
-print("Done!")
+    atoms.set_calculator(calc)
+    
+    print(atoms.get_potential_energy())
+    #print(atoms.get_forces())
+    print("Done!")
+
+
+if __name__ == '__main__':
+    main()
